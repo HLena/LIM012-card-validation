e677f798c03a8f00ecad4c483e07978e976ad402
GHlena <ghlena@pop-os.localdomain>
2020-02-12 19:08:05 -0500

    Metodos isValid y maskify  implementadas + testeo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+/* eslint-disable no-console */
 import validator from './validator.js';
 
-console.log(validator);
+// let nameOwner = document.getElementById("inputName");
+// let cardNumber = document.getElementById("inputCardNumber");
+// let codeSecurity = document.getElementById("inputCode");
+// let dateExpiry = document.getElementById("inputDateExpiry");
+// let continueBtn = document.getElementById("continueBtn");
+
+// console.log(validator);
+
+// const card = Object.create(validator);
+
+// console.log(card.isValid("4083952015263"));
+// console.log(card.isValid("79927398713"));
+// console.log(card.isValid("1234567890"));
+
+// console.log(card.maskify("4556364607935616"));
+// console.log(card.maskify("1"));
+// console.log(card.maskify("helloworld"));
+
+// continueBtn.addEventListener("click",function(){
+//       const response = card.isValid(cardNumber.value);
+//       if(response)
+//             console.log("Es valida");
+//       else
+//             console.log("No es valida");
+// });
+
+console.log(validator)
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
+/* eslint-disable no-console */
 const validator = {
-  // ...
+      // isValid: metodo para validar tarjeta
+      isValid : function(cardNumber){
+            let sum = 0;
+            let currentDigit = 0;
+
+            for(let i = cardNumber.length - 1; i >=0 ; i--){
+                  currentDigit = parseInt(cardNumber[i],10);
+                  if(i % 2 == 1){
+                        //console.log("["+i+"]=> "+ currentDigit);
+                        currentDigit = currentDigit * 2;
+                        //console.log("= = > "+ currentDigit);
+                        if(currentDigit > 9){
+                              currentDigit = 1 + (currentDigit % 10);
+                              //console.log("= = => "+ currentDigit);
+                        }
+                  }
+                  sum += currentDigit;            
+            }            
+            if(sum % 10 === 0){
+                  return true;
+            }
+            else{
+                  return false;
+            }
+      },      
+      maskify : function(cardNumber){
+           const lastDigit = 4;
+           if(cardNumber.length > lastDigit){
+                  let vec = cardNumber.split("");
+                  // console.log(vec);
+                  for(let i = (cardNumber.length - lastDigit)-1; i >= 0; i--){
+                        vec[i] = "#";
+                 }
+            //      console.log(vec.join(''));
+                 return vec.join('');
+           }
+      //      console.log(cardNumber);
+           return cardNumber;           
+      }
 };
 
 export default validator;
