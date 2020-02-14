764cbb456d333bf9f0189e14e4b8b588b3d4f693
GHlena <ghlena@pop-os.localdomain>
2020-02-13 22:15:04 -0500

    Validaciones de inputs + funcionamiento en la visualizacion de la tarjeta

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ const cardObj = {
       cvv : '***'
 }
 
+
+//Elementos Input
 let inputName = document.getElementById("input-name");
 let inputNumber = document.getElementById("input-number");
 let inputDate = document.getElementById("input-date");
 // let inputCvv = document.getElementById("input-cvv");
-
 let validarBtn = document.getElementById("validarBtn");
 let rptsLabel = document.getElementById("rpts");
 
@@ -20,44 +21,122 @@ let rptsLabel = document.getElementById("rpts");
 let cardNumber = document.getElementById("card-number");
 let cardName = document.getElementById("card-name");
 let cardDate = document.getElementById("card-date");
-// let cardCvv = document.getElementById("card-cvv");
 
 
+let alertWindows = document.getElementById("alert");
 
-const card = Object.create(validator);
-const cardObject = Object.create(cardObj);
-
-inputNumber.addEventListener("input",function(){
-      cardNumber.innerHTML = "" + card.maskify(inputNumber.value);
-      if(inputNumber.value.length === 0)
-            cardNumber.innerHTML = cardObject.number;
-});
-
-inputName.addEventListener("input",function(){
-      cardName.innerHTML = "" + card.maskify(inputName.value);
-      if(inputName.value.length === 0)
-            cardName.innerHTML = cardObject.name;
-});
-
-inputDate.addEventListener("input",function(){
-      cardDate.innerHTML = "" + inputDate.value;
-      if(inputDate.value.length === 0)
-            cardDate.innerHTML = cardObject.date;
-});
-
-validarBtn.addEventListener("click",function(){
-      const number = cardNumber.value;
-      const response = card.isValid(number);
-      
-      if(number.length > 0){
-            if(response === true)
-                  rptsLabel.innerHTML = "Es Valida";                  
-            else
-                  rptsLabel.innerHTML = "No es Valida";
-      }else{ 
-            rptsLabel.innerHTML = "Ingrese Numero"   ;
+// Eventos para modificar Tarjeta
+inputNumber.addEventListener("keydown",(event) =>
+{      
+      let ch = String.fromCharCode(event.which);
+      console.log("codigo: "+event.which);
+      if((/[0-9]/.test(ch)) || event.which === 8)
+      {
+            inputNumber.addEventListener("input",() =>
+            {
+                  console.log("evento input");
+                  cardNumber.innerHTML = "" + validator.maskify(inputNumber.value); 
+                  console.log("valor: "+inputNumber.value);
+                  if(inputNumber.value.length === 0)
+                        cardNumber.innerHTML = cardObj.number;       
+            });
       }
-                  
+      else
+      {
+           event.preventDefault(); 
+      }
+});
+
+inputName.addEventListener("keydown",(event) => {      
+      let ch = String.fromCharCode(event.which);
+      if((/[a-zA-Z]/.test(ch)) || event.which === 8)
+      {
+            inputName.addEventListener("input",() =>
+            {
+                  console.log("evento input");
+                  cardName.innerHTML = "" + validator.maskify(inputName.value); 
+                  console.log("valor: "+inputName.value);
+                  if(inputName.value.length === 0)
+                        cardName.innerHTML = cardObj.name;       
+            });
+      }
+      else
+      {
+            event.preventDefault();
+      }
+});
+
+inputDate.addEventListener("keydown",(event) => 
+{     //Formato MM/YY
+      let len = inputDate.value.length;
+      let digit = String.fromCharCode(event.which);      
+      if(len === 0 && !(/[0-1]/.test(digit)))
+      {      
+            event.preventDefault();
+      }
+      else
+      {            
+            if(len === 1 && !(event.which === 8 ))
+            {
+                  if((inputDate.value[0] === "0" && !(/[1-9]/.test(digit))) || (inputDate.value[0] === "1" && !(/[0-2]/.test(digit))))
+                  {                        
+                        event.preventDefault();
+                  }
+                  else
+                  {
+                        cardDate.innerHTML = "" + inputDate.value;                                                                  
+                  }
+
+            }
+            else if(len === 2 && !(event.which === 8 ))
+            {     
+                  console.log("valor: " + inputDate.value);
+                  inputDate.value += "/";                      
+            }
+            else if(event.which === 8 || (/[0-9]/.test(digit)))
+            {                  
+                  inputDate.addEventListener("input",() => 
+                  {
+                        cardDate.innerHTML = "" + inputDate.value;                                          
+                        if(inputDate.value.length === 0)
+                              cardDate.innerHTML = cardObj.date;  
+                        
+                  });                  
+            }
+            else
+            {                  
+                  event.preventDefault();      
+            }
+      }      
+});
+
+
+//Evento para valida tarjeta
+validarBtn.addEventListener("click",() => 
+{
+      let number = inputNumber.value;      
+      const response = validator.isValid(number);
+
+      if(number.length > 0)
+      {
+            if(response === true)
+            {
+                  // alertWindows.style.border = "#49B607";
+                  rptsLabel.innerHTML = "Tarjeta Válida";  
+                  rptsLabel.style.color = "#49B607"
+            }
+            else
+            {
+                  // alertWindows.style.borderColor = "#FF5733";
+                  rptsLabel.innerHTML = "Tarjeta Inválida";
+                  rptsLabel.style.color = "#FF5733";           
+            }
+      }
+      else
+      { 
+            rptsLabel.innerHTML = "Ingrese Número de Tarjeta";
+            rptsLabel.style.color = "#F1CC22"; 
+      }                  
 });
 
 // console.log(validator)
--- src/validator.js
+++ src/validator.js
@@ -1,35 +1,36 @@
 /* eslint-disable no-console */
 const validator = {
       // isValid: metodo para validar tarjeta
-      isValid : (cardNumber) => {
+      isValid : (cardNumber) => 
+      {
             let sum = 0;
             let currentDigit = 0;
 
-            for(let i = cardNumber.length - 1; i >= 0 ; i--){
+            for(let i = cardNumber.length - 1; i >= 0 ; i--)
+            {
                   currentDigit = parseInt(cardNumber[i],10);
-                  if(i % 2 === 1){                        
+                  if(i % 2 === 1)
+                  {                        
                         currentDigit = currentDigit * 2;                        
-                        if(currentDigit > 9){
-                              currentDigit = 1 + (currentDigit % 10);                              
-                        }
+                        if(currentDigit > 9)                  
+                              currentDigit = 1 + (currentDigit % 10);                        
                   }
                   sum += currentDigit;            
             }            
-            if(sum % 10 === 0){
+            if(sum % 10 === 0)            
                   return true;
-            }
-            else{
-                  return false;
-            }
+            else
+                  return false;  
       },      
-      maskify : (cardNumber) => {
+      maskify : (cardNumber) => 
+      {
            const lastDigit = 4;
-           if(cardNumber.length > lastDigit){
-                  let vec = cardNumber.split("");                  
-                  for(let i = (cardNumber.length - lastDigit)-1; i >= 0; i--){
-                        vec[i] = "#";
-                 }            
-                 return vec.join('');
+           if(cardNumber.length > lastDigit)
+           {
+                  let vec = cardNumber.split(""); 
+                  for(let i = (cardNumber.length - lastDigit)-1; i >= 0; i--)            
+                        vec[i] = "#";            
+                  return vec.join('');
            }      
            return cardNumber;           
       }
