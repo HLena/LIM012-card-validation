4bb6b2dd646bd0d35aca8af77b6aeccd7cb26c54
Elena <karengvcs@gmail.com>
2020-07-18 23:21:42 -0500

    Delete inncesary images

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 import validator from './validator.js';
 
 const cardObj = {
-      number: '**** **** **** ****',
+      number: '#### #### #### ####',
       name: 'NOMBRE COMPLETO',
       date: 'MM/AA',
-      cvv : '***',
+      cvv : '###',
       signature :''
 }
 const KEY_DELETE = 8;
@@ -17,7 +17,7 @@ let cardNumber = document.getElementById("card-number");
 let cardName = document.getElementById("card-name");
 let cardDate = document.getElementById("card-date");
 let cardCvv = document.getElementById("card-cvv");
-let cardSignature = document.getElementsByClassName("signature-panel");
+let cardSignature = document.getElementById("signature-panel");
 
 //Elementos del formulario(Inputs)
 let inputName = document.getElementById("input-name");
@@ -28,13 +28,15 @@ let validateBtn = document.getElementById("validate-btn");
 
 
 //Elementos del mensaje(alert/valido/invalido)
-let formBox = document.getElementsByClassName("payment-form-box");            
-let alertBox = document.getElementsByClassName("alert");
-let alertImg = document.getElementsByClassName("icon-check");
-let alertMessage = document.getElementById("rpts");
+let formBox = document.querySelector("#form");            
+let alertBox = document.querySelector(".alert");
+let alertIcon = document.querySelector(".icon-validation");
+const alertMessage = document.getElementById("rpts");
+let resultMessage = document.querySelector(".result");
 let otherCardBtn = document.getElementById("btn-other-card");
 
-
+const valid = 'bxs-check-circle';
+const invalid = 'bxs-x-circle';
 // Eventos para modificar Tarjeta
 inputNumber.addEventListener("keydown",(event) =>
 {      
@@ -63,7 +65,7 @@ inputName.addEventListener("keydown",(event) => {
             inputName.addEventListener("input",() =>
             {                  
                   cardName.textContent = "" + (inputName.value).toUpperCase();
-                  cardSignature[0].textContent = "" + inputName.value;                                     
+                  cardSignature.textContent = "" + inputName.value;                                     
                   if(inputName.value.length === 0)
                         cardName.textContent = cardObj.name;       
             });
@@ -138,37 +140,40 @@ inputDate.addEventListener("keydown",(event) =>
 //Evento para validar tarjeta
 validateBtn.addEventListener("click",() => 
 {
+      alertMessage.innerHTML= '';
       if(face === false){
             card.style.transform = "rotateY(360deg)";            
             face = true;
       }      
-      alertBox[0].style.display = 'block';      
-      alertMessage.style.display = 'block';
       if(inputNumber.value.length > 0 && inputName.value.length > 0 && inputDate.value.length > 0 && inputCvv.value.length > 0)
       {            
             const response = validator.isValid(inputNumber.value);            
-            formBox[0].style.display = 'none';            
-            alertBox[0].style.heigth = '5em';
-            alertImg[0].style.display = "inline-block";            
+            formBox.classList.add('hidden');            
+            alertBox.classList.remove('hidden')                
             otherCardBtn.style.display = "inline-block";
             if(response === true)
             {                        
-                  alertImg[0].src = "img/correct-check.png";
-                  alertMessage.textContent = "Tarjeta Válida";  
-                  alertMessage.style.color = "#49B607";
-                  alertMessage.style.fontSize = "2em";
+                  if (alertIcon.classList.contains(invalid)) alertIcon.classList.remove(invalid);
+                  alertIcon.classList.toggle(valid);
+                  resultMessage.textContent = "Valid Card";  
+                  resultMessage.style.color = "#49B607";
+                  alertIcon.style.color = "#49B607";
+                  // resultMessage.style.fontSize = "2em";
             }
             else
             {     
-                  alertImg[0].src = "img/wrong-check.png";                
-                  alertMessage.textContent = "Tarjeta Inválida";
-                  alertMessage.style.color = "#FF5733";      
-                  alertMessage.style.fontSize = "2em";
+                  if (alertIcon.classList.contains(valid)) alertIcon.classList.remove(valid);
+                  alertIcon.classList.toggle(invalid);
+                  // alertIcon.src = "img/wrong-check.png";                
+                  resultMessage.textContent = "invalid Card";
+                  resultMessage.style.color = "#FF5733"; 
+                  alertIcon.style.color = "#FF5733"; 
+                  // resultMessage.style.fontSize = "2em";
             }
             
       }else
       {            
-            alertMessage.textContent = "Complete todos los campos";
+            alertMessage.innerHTML = "Complete all the input fields <i class='bx bxs-message-square-error' ></i>";
             alertMessage.style.color = "#F1CC22";
             otherCardBtn.style.display = "none"; 
       }
@@ -184,14 +189,16 @@ let cleanInputs = () => {
       cardName.textContent = cardObj.name;
       cardDate.textContent = cardObj.date;
       cardCvv.textContent = cardObj.cvv;
-      cardSignature[0].textContent = cardObj.signature;
+      cardSignature.textContent = cardObj.signature;
 }
 
 
 otherCardBtn.addEventListener('click', () => {
-      alertBox[0].style.display = 'none';      
-      alertImg[0].style.display = "none";
-      formBox[0].style.display = 'block';  
+      alertIcon.style.display = 'none';      
+      alertIcon.style.display = "none";
+      formBox.classList.remove('hidden');
+      alertBox.classList.add('hidden');
+      alertMessage.innerHTML= '';
       cleanInputs();
 
 
